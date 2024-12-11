a27cd81e26a8b2c95ae39660d577fb86104c93ad
andriusmw <andriusmw@yahoo.es>
2024-12-11 09:39:16 +0100

    materia ui?

--- ANGULAR17-NGRX/src/app/app.config.ts
+++ ANGULAR17-NGRX/src/app/app.config.ts
@@ -6,7 +6,8 @@ import { provideClientHydration } from '@angular/platform-browser';
 import { provideStore } from '@ngrx/store';
 import { provideEffects } from '@ngrx/effects';
 import { provideStoreDevtools } from '@ngrx/store-devtools';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration(), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() })]
+  providers: [provideRouter(routes), provideClientHydration(), provideStore(), provideEffects(), provideStoreDevtools({ maxAge: 25, logOnly: !isDevMode() }), provideAnimationsAsync()]
 };
--- ANGULAR17-NGRX/src/app/component/menu-header/menu-header.component.ts
+++ ANGULAR17-NGRX/src/app/component/menu-header/menu-header.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { MaterialModule } from '../../../_module/Material.Module';
 
 @Component({
   selector: 'app-menu-header',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, MaterialModule],
   templateUrl: './menu-header.component.html',
   styleUrl: './menu-header.component.scss'
 })
 export class MenuHeaderComponent {
 
 }
+
